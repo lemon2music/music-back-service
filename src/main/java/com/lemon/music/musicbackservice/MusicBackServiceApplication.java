b4ebff94cb32aecba1c145625ccbd5aa9bf999b1
jeho0815 <jeho0815@163.com>
2026-07-06 20:28:49 +0800

    项目初始化

---
+++ src/main/java/com/lemon/music/musicbackservice/MusicBackServiceApplication.java
@@ -0,0 +1,13 @@
+package com.lemon.music.musicbackservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MusicBackServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MusicBackServiceApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/lemon/music/musicbackservice/MusicBackServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lemon.music.musicbackservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MusicBackServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
